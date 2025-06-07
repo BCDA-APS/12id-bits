a60dd7a5e6b3e773563567dd87358b96d5221f26
Pete R Jemian <prjemian@users.noreply.github.com>
2025-06-06 21:58:55 -0500

    MNT copy My12IdPssShutter from USAXS-bits

--- src/id12_common/devices/shutters.py
+++ src/id12_common/devices/shutters.py
@@ -1,7 +1,14 @@
 """Custom shutter support for 12ID."""
 
-from apstools.devices.shutters import ApsPssShutter
+from typing import List
+from typing import Union
 
+from apstools.devices import ApsPssShutterWithStatus
+from apstools.devices.shutters import ApsPssShutter
+from ophyd import Component
+from ophyd import EpicsSignal
+from ophyd import EpicsSignalRO
+from ophyd import FormattedComponent
 
 class ID12_ApsPssShutter(ApsPssShutter):
     """Adjust the delay time as a kwarg."""
@@ -10,3 +17,26 @@ class ID12_ApsPssShutter(ApsPssShutter):
         """."""
         self.delay_s = delay_s
         super().__init__(prefix, *args, **kwargs)
+
+
+class My12IdPssShutter(ApsPssShutterWithStatus):
+    """
+    Controls a single APS PSS shutter at 12IDE (from USAXS-bits).
+
+    ======  =========  =====
+    action  PV suffix  value
+    ======  =========  =====
+    open    _opn       1
+    close   _cls       1
+    ======  =========  =====
+    """
+
+    # bo records that reset after a short time, set to 1 to move
+    open_signal: Component[EpicsSignal] = Component(EpicsSignal, "_opn")
+    close_signal: Component[EpicsSignal] = Component(EpicsSignal, "_cls")
+    # bi record ZNAM=OFF, ONAM=ON
+    pss_state: FormattedComponent[EpicsSignalRO] = FormattedComponent(
+        EpicsSignalRO, "{self.state_pv}"
+    )
+    pss_state_open_values: List[Union[int, str]] = [1, "ON"]
+    pss_state_closed_values: List[Union[int, str]] = [0, "OFF"]
