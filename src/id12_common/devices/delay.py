3ccc7483e65937e3adc7daa85094d71c2f155dca
S12 Service Acct <s12idc@green.xray.aps.anl.gov>
2025-06-12 09:30:05 -0500

    MNT #1 implement EPICS support for DG645

---
+++ src/id12_common/devices/delay.py
@@ -0,0 +1,18 @@
+"""
+Changes to apstools DG645 support.
+
+.. seealso:: https://github.com/BCDA-APS/apstools/issues/1109
+"""
+
+from apstools.devices.delay import DG645Delay as aps_DG645Delay
+from apstools.devices.delay import EpicsSignalWithIO
+from ophyd import Component as Cpt, EpicsSignal
+
+
+class DG645Delay(aps_DG645Delay):
+    """Changes per apstools issue #1109."""
+
+    burst_mode = Cpt(EpicsSignalWithIO, "BurstModeB", kind="config")
+    burst_T0 = Cpt(EpicsSignalWithIO, "BurstConfigB", kind="config")
+    # Initiate or arm a trigger
+    trigger_delay = Cpt(EpicsSignal, "TriggerDelayBO", kind="omitted")
