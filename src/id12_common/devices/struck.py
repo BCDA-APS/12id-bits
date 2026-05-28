3d9d397aa0dda9650b36888f2a2193c44c3b1c7d
mdecarabas <codrea.eric@gmail.com>
2026-05-28 13:57:48 -0500

    added scaler1

--- src/id12_common/devices/struck.py
+++ src/id12_common/devices/struck.py
@@ -1,9 +1,12 @@
 """Struck3820 at 12-ID is missing a clock frequency PV."""
 
 from apstools.devices.struck3820 import Struck3820
+from ophyd import Component
+from ophyd import EpicsSignal
 
 
 class Struck(Struck3820):
     """Override for Struck3820 at 12ID."""
 
     clock_frequency = None
+    scaler = Component(EpicsSignal, "scaler1")
